1b9ee87b8da1402ffd1df59dac753f4dd155cc2c
Sriram Govindan <marirs@gmail.com>
2021-04-21 10:13:50 +0530

    example

--- examples/unshorten.rs
+++ examples/unshorten.rs
@@ -2,36 +2,11 @@ use core::time::Duration;
 use urlexpand;
 
 fn main() {
-    // let url = "https://bit.ly/3alqLKi";
-    // println!("{}\nis_shortened? {}\nExpanded URL = {:?}",
-    //     url,
-    //     urlexpand::is_shortened(url),
-    //     urlexpand::unshorten(url, Some(Duration::new(30,0)))
-    // );
-    // println!();
-    //
-    // let url = "https://bit.ly/3dsTDlV";
-    // println!("{}\nis_shortened? {}\nExpanded URL = {:?}",
-    //      url,
-    //      urlexpand::is_shortened(url),
-    //      urlexpand::unshorten(url, Some(Duration::new(30,0)))
-    // );
-    // println!();
-    //
-    let url = "https://tinyurl.com/2j582c6a";
+    let url = "https://bit.ly/3alqLKi";
     println!("{}\nis_shortened? {}\nExpanded URL = {:?}",
-             url,
-             urlexpand::is_shortened(url),
-             urlexpand::unshorten(url, Some(Duration::new(30,0)))
+        url,
+        urlexpand::is_shortened(url),
+        urlexpand::unshorten(url, Some(Duration::new(30,0)))
     );
     println!();
-
-    // let url = "https://t.co/bYeHhy9kAU";
-    // println!("{}\nis_shortened? {}\nExpanded URL = {:?}",
-    //          url,
-    //          urlexpand::is_shortened(url),
-    //          urlexpand::unshorten(url, Some(Duration::new(30,0)))
-    // );
-    // println!();
-
 }
