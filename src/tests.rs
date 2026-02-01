356e47637d1aaa1afc7ab0466e605489836a9e14
SG <marirs@gmail.com>
2026-02-01 14:59:04 +0400

    tests rewrite

--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,84 @@
 use super::{is_shortened, unshorten, validate};
 #[cfg(feature = "blocking")]
-use super::unshorten_blocking;
+use super::unshorten_async;
+use std::time::Duration;
 
-use paste::paste;
+/// Test data for URL expansion
+struct TestCase {
+    name: &'static str,
+    short_url: &'static str,
+    expected_contains: &'static str,
+    exact_match: bool,
+}
 
+impl TestCase {
+    const fn new(name: &'static str, short_url: &'static str, expected: &'static str) -> Self {
+        Self {
+            name,
+            short_url,
+            expected_contains: expected,
+            exact_match: true,
+        }
+    }
+
+    const fn starts_with(name: &'static str, short_url: &'static str, expected: &'static str) -> Self {
+        Self {
+            name,
+            short_url,
+            expected_contains: expected,
+            exact_match: false,
+        }
+    }
+}
+
+const TEST_CASES: &[TestCase] = &[
+    TestCase::new("bit_ly", "https://bit.ly/3alqLKi", "https://www.google.com/"),
+    TestCase::new("b_link", "https://b.link/cx2x2l", "http://www.google.com/"),
+    TestCase::new("cli_re", "https://cli.re/wxbz38", "https://www.google.com/"),
+    TestCase::new("cutt_ly", "https://cutt.ly/tvDqE79", "https://www.google.com/"),
+    TestCase::new("fb_me", "https://fb.me/mashable", "https://www.facebook.com/mashable"),
+    TestCase::new("git_io", "https://git.io/JOiM6", "https://www.github.com/marirs/urlexpand"),
+    TestCase::starts_with("hmm_rs", "http://hmm.rs/Hangfire.PostgreSql", "https://github.com/"),
+    TestCase::new("kutt_it", "https://kutt.it/jO2XmP", "https://www.google.com/"),
+    TestCase::starts_with("lnkd_in", "https://lnkd.in/g3T6zqDE", "https://paulgraham.com"),
+    TestCase::new("mlz_la", "https://mzl.la/3eqJ565", "http://support.mozilla.org/"),
+    TestCase::new("ow_ly", "http://ow.ly/j9qh7", "http://t.co/cAcQV4QTOS"),
+    TestCase::new("plu_sh", "https://plu.sh/xnwb8", "https://www.google.com/"),
+    TestCase::new("rb_gy", "https://rb.gy/ciq6si", "https://google.com/"),
+    TestCase::new("rlu_ru", "https://rlu.ru/1B5", "https://www.google.com"),
+    TestCase::starts_with("surl_li", "surl.li/aap", "http://google.com"),
+    TestCase::new("t_ly", "https://t.ly/2ESW", "https://www.google.com/"),
+    TestCase::new("tny_sh", "https://tny.sh/5C3X9Ss", "http://google.com/"),
+    TestCase::starts_with("trib_al", "https://trib.al/YKNecc2", "https://mashable.com/article"),
+];
+
+/// Helper function to test URL expansion
+fn test_expansion(result: super::Result<String>, test_case: &TestCase) -> std::result::Result<(), String> {
+    match result {
+        Ok(expanded_url) => {
+            let success = if test_case.exact_match {
+                expanded_url == test_case.expected_contains
+            } else {
+                expanded_url.starts_with(test_case.expected_contains)
+            };
+
+            if success {
+                Ok(())
+            } else {
+                Err(format!(
+                    "{}: Expected {} '{}', got '{}'",
+                    test_case.name,
+                    if test_case.exact_match { "exact match" } else { "starts with" },
+                    test_case.expected_contains,
+                    expanded_url
+                ))
+            }
+        }
+        Err(e) => Err(format!("{}: Request failed: {:?}", test_case.name, e)),
+    }
+}
+
+// Basic unit tests
 #[test]
 fn test_validate() {
     assert!(validate("bit.ly").is_some());
@@ -22,381 +97,104 @@ fn test_is_shortened() {
     assert!(!is_shortened(url));
 }
 
-macro_rules! test_shorten_link {
-    ($t_name:ident, $s_url:expr, $op:ident, $e_url:expr) => {
-        #[tokio::test]
-        async fn $t_name() {
-            let expanded_url = unshorten($s_url, None).await;
-            assert_eq!(
-                expanded_url.as_ref().map(|x| x.$op($e_url)),
-                Ok(true),
-                "{}: {:?} {} {}",
-                $s_url,
-                expanded_url,
-                stringify!($op),
-                $e_url
-            );
-        }
+// Async tests (always available)
+#[cfg(not(feature = "blocking"))]
+mod async_tests {
+    use super::*;
 
-        // until std::concat_idents stablizes
-        paste! {
-            #[test]
-            #[cfg(feature = "blocking")]
-            fn [<$t_name _blocking>]() {
-                let expanded_url = unshorten_blocking($s_url, None);
-                assert_eq!(
-                    expanded_url.as_ref().map(|x| x.$op($e_url)),
-                    Ok(true),
-                    "{}: {:?} {} {}",
-                    $s_url,
-                    expanded_url,
-                    stringify!($op),
-                    $e_url
-                );
+    #[tokio::test]
+    async fn test_async_expansions() {
+        for test_case in TEST_CASES {
+            let result = unshorten(test_case.short_url, Some(Duration::from_secs(10))).await;
+            if let Err(msg) = test_expansion(result, test_case) {
+                panic!("{}", msg);
             }
         }
-    };
+    }
+
+    #[tokio::test]
+    async fn test_async_with_timeout() {
+        let result = unshorten("https://bit.ly/3alqLKi", Some(Duration::from_secs(5))).await;
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_async_no_timeout() {
+        let result = unshorten("https://bit.ly/3alqLKi", None).await;
+        assert!(result.is_ok());
+    }
 }
 
-test_shorten_link!(
-    test_bit_ly,
-    "https://bit.ly/3alqLKi",
-    eq,
-    "https://www.google.com/"
-);
+// Tests when blocking feature is enabled
+#[cfg(feature = "blocking")]
+mod blocking_tests {
+    use super::*;
 
-test_shorten_link!(
-    test_aliexpress,
-    "https://s.click.aliexpress.com/e/_c3iB26TD",
-    starts_with,
-    "https://www.aliexpress.com/item/"
-);
+    #[test]
+    fn test_blocking_expansions() {
+        for test_case in TEST_CASES {
+            let result = unshorten(test_case.short_url, Some(Duration::from_secs(10)));
+            if let Err(msg) = test_expansion(result, test_case) {
+                panic!("{}", msg);
+            }
+        }
+    }
 
-test_shorten_link!(
-    test_b_link,
-    "https://b.link/cx2x2l",
-    eq,
-    "http://www.google.com/"
-);
+    #[test]
+    fn test_blocking_with_timeout() {
+        let result = unshorten("https://bit.ly/3alqLKi", Some(Duration::from_secs(5)));
+        assert!(result.is_ok());
+    }
 
-test_shorten_link!(
-    test_buff_ly,
-    "https://buff.ly/1GYcFvQ",
-    starts_with,
-    "https://blog.bufferapp.com/url-shorteners?"
-);
+    #[test]
+    fn test_blocking_no_timeout() {
+        let result = unshorten("https://bit.ly/3alqLKi", None);
+        assert!(result.is_ok());
+    }
 
-test_shorten_link!(
-    test_cli_re,
-    "https://cli.re/wxbz38",
-    eq,
-    "https://www.google.com/"
-);
+    #[tokio::test]
+    async fn test_async_still_available() {
+        // Test that async version is still available when blocking feature is enabled
+        let result = unshorten_async("https://bit.ly/3alqLKi", Some(Duration::from_secs(5))).await;
+        assert!(result.is_ok());
+    }
+}
 
-test_shorten_link!(
-    test_chollo_to,
-    "https://chollo.to/s1q4u",
-    starts_with,
-    "https://www.chollometro.com/ofertas/xiaomi-mi-band-6"
-);
+// Integration tests that work for both configurations
+#[cfg(feature = "blocking")]
+mod integration_tests {
+    use super::*;
 
-test_shorten_link!(
-    test_cutt_ly,
-    "https://cutt.ly/tvDqE79",
-    eq,
-    "https://www.google.com/"
-);
+    #[tokio::test]
+    async fn test_async_available_with_blocking_feature() {
+        // Test that async version is still available when blocking feature is enabled
+        let result = unshorten_async("https://bit.ly/3alqLKi", Some(Duration::from_secs(5))).await;
+        assert!(result.is_ok());
+    }
+}
 
-test_shorten_link!(
-    test_db_tt,
-    "https://db.tt/cchelp",
-    starts_with,
-    "https://www.dropbox.com/"
-);
+// Error handling tests
+mod error_tests {
+    use super::*;
 
-test_shorten_link!(
-    test_fb_me,
-    "https://fb.me/mashable",
-    eq,
-    "https://www.facebook.com/mashable"
-);
+    #[cfg(not(feature = "blocking"))]
+    #[tokio::test]
+    async fn test_async_invalid_url() {
+        let result = unshorten("https://example.com/not-a-shortener", Some(Duration::from_secs(5))).await;
+        assert!(result.is_err());
+    }
 
-test_shorten_link!(
-    test_git_io,
-    "https://git.io/JOiM6",
-    eq,
-    "https://www.github.com/marirs/urlexpand"
-);
+    #[cfg(feature = "blocking")]
+    #[test]
+    fn test_blocking_invalid_url() {
+        let result = unshorten("https://example.com/not-a-shortener", Some(Duration::from_secs(5)));
+        assert!(result.is_err());
+    }
 
-test_shorten_link!(
-    test_goo_gl,
-    "https://goo.gl/cvSjeY",
-    starts_with,
-    "http://m.axisbank.com/"
-);
-
-test_shorten_link!(
-    test_gns_io,
-    "https://gns.io/1RQl2",
-    starts_with,
-    "https://google.com/"
-);
-
-test_shorten_link!(
-    test_hmm_rs,
-    "http://hmm.rs/Hangfire.PostgreSql",
-    starts_with,
-    "https://github.com/"
-);
-
-test_shorten_link!(
-    test_hyperurl_co,
-    "http://hyperurl.co/qicb73",
-    eq,
-    "https://www.google.com/"
-);
-
-test_shorten_link!(
-    test_ity_im,
-    "http://ity.im/U8re4",
-    starts_with,
-    "https://www.google.com/search?q=rust&"
-);
-
-test_shorten_link!(
-    test_iz4_short_gy,
-    "https://iz4.short.gy/mr7KcJ",
-    starts_with,
-    "https://www.google.com"
-);
-
-test_shorten_link!(
-    test_kutt_it,
-    "https://kutt.it/jO2XmP",
-    eq,
-    "https://www.google.com/"
-);
-
-test_shorten_link!(
-    test_ldn_im,
-    "http://ldn.im/1pNey",
-    starts_with,
-    "https://google.com/"
-);
-
-test_shorten_link!(
-    test_linklyhq_com,
-    "https://l.linklyhq.com/l/QebZ",
-    starts_with,
-    "https://www.google.com/"
-);
-
-test_shorten_link!(
-    test_lnkd_in,
-    "https://lnkd.in/g3T6zqDE",
-    starts_with,
-    "https://paulgraham.com"
-);
-
-test_shorten_link!(
-    test_mlz_la,
-    "https://mzl.la/3eqJ565",
-    eq,
-    "http://support.mozilla.org/"
-);
-
-test_shorten_link!(
-    test_ow_ly,
-    "http://ow.ly/j9qh7",
-    eq,
-    "http://t.co/cAcQV4QTOS"
-);
-
-test_shorten_link!(
-    test_plu_sh,
-    "https://plu.sh/xnwb8",
-    eq,
-    "https://www.google.com/"
-);
-
-test_shorten_link!(
-    test_qr_ae,
-    "http://qr.ae/7FQS9",
-    starts_with,
-    "http://www.quora.com"
-);
-
-test_shorten_link!(
-    test_rb_gy,
-    "https://rb.gy/ciq6si",
-    eq,
-    "https://google.com/"
-);
-
-test_shorten_link!(
-    test_rebrand_ly,
-    "https://rebrand.ly/dp8cuo0",
-    eq,
-    "https://www.google.com/"
-);
-
-test_shorten_link!(
-    test_rlu_ru,
-    "https://rlu.ru/1B5",
-    eq,
-    "https://www.google.com"
-);
-
-test_shorten_link!(
-    test_rotf_lol,
-    "https://rotf.lol/4scu3nzz",
-    eq,
-    "https://google.com/"
-);
-
-test_shorten_link!(
-    test_shorturl_at,
-    "https://shorturl.at/kmrEO",
-    eq,
-    "https://www.google.com"
-);
-
-test_shorten_link!(
-    test_split_to,
-    "https://split.to/V1ZhFut",
-    starts_with,
-    "https://google.com"
-);
-
-test_shorten_link!(
-    test_smc_sg,
-    "https://smu.sg/4l4",
-    starts_with,
-    "https://google.com/"
-);
-
-test_shorten_link!(
-    test_snip_ly,
-    "snip.ly/soyummy-cookbook",
-    starts_with,
-    "https://www.soyummystore.com"
-);
-
-test_shorten_link!(
-    test_surl_ly_redirect,
-    "surl.li/cgonw",
-    starts_with,
-    "https://bing.com/"
-);
-
-test_shorten_link!(
-    test_surl_ly,
-    "surl.li/aap",
-    starts_with,
-    "http://google.com"
-);
-
-test_shorten_link!(
-    test_t_co,
-    "https://t.co/bYeHhy9kAU",
-    eq,
-    "https://www.youtube.com/watch?v=x6QZn9xiuOE"
-);
-
-test_shorten_link!(
-    test_t_ly,
-    "https://t.ly/2ESW",
-    eq,
-    "https://www.google.com/"
-);
-
-test_shorten_link!(
-    test_t2m_io,
-    "https://t2m.io/SSQhKqJ2",
-    eq,
-    "https://www.google.com/"
-);
-
-test_shorten_link!(
-    test_tiny_cc,
-    "https://tiny.cc/5ocwtz",
-    eq,
-    "https://www.google.com/"
-);
-
-test_shorten_link!(
-    test_tinurl_com,
-    "https://tinyurl.com/2j582c6a",
-    eq,
-    "https://google.com"
-);
-
-test_shorten_link!(
-    test_tny_sh,
-    "https://tny.sh/5C3X9Ss",
-    eq,
-    "http://google.com/"
-);
-
-test_shorten_link!(
-    test_tr_im,
-    "https://tr.im/1iMz2",
-    starts_with,
-    "https://google.com/"
-);
-
-test_shorten_link!(
-    test_trib_al,
-    "https://trib.al/YKNecc2",
-    starts_with,
-    "https://mashable.com/article"
-);
-
-test_shorten_link!(
-    test_u_to,
-    "https://u.to/P05FGw",
-    starts_with,
-    "https://www.google.com/search?q=rust"
-);
-
-test_shorten_link!(
-    test_v_gd,
-    "https://v.gd/6H6dYQ",
-    eq,
-    "https://www.google.com/"
-);
-
-test_shorten_link!(
-    test_virg_in,
-    "https://virg.in/9sj",
-    eq,
-    "https://www.virginactive.co.za/quest"
-);
-
-test_shorten_link!(
-    test_vzturl_com,
-    "https://vzturl.com/bqd20",
-    starts_with,
-    "https://www.amazon.com/Sabrent-Thunderbolt-"
-);
-
-test_shorten_link!(
-    test_waa_ai,
-    "https://waa.ai/muZV",
-    starts_with,
-    "https://www.google.com/search?q=rust"
-);
-
-test_shorten_link!(
-    test_yourwish_es,
-    "http://yourwish.es/oxgyc",
-    starts_with,
-    "https://www.amazon.com/Sabrent-Thunderbolt"
-);
-
-test_shorten_link!(
-    test_zpr_io,
-    "http://zpr.io/nniJB",
-    eq,
-    "http://www.archiveteam.org/"
-);
+    #[cfg(feature = "blocking")]
+    #[tokio::test]
+    async fn test_async_invalid_url_with_blocking_feature() {
+        let result = unshorten_async("https://example.com/not-a-shortener", Some(Duration::from_secs(5))).await;
+        assert!(result.is_err());
+    }
+}
